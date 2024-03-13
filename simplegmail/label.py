327cf07142baeac113f61f06fd9130b88b051ffe
Matt Black <github@mafro.net>
2024-03-13 14:41:53 +1100

    Use a dataclass for Label

--- simplegmail/label.py
+++ simplegmail/label.py
@@ -1,50 +1,9 @@
-"""
-File: label.py
---------------
-Gmail reserved system labels and the Label class.
+import dataclasses
 
-"""
-
-
+@dataclasses.dataclass
 class Label:
-    """
-    A Gmail label object.
-
-    This class should not typically be constructed directly but rather returned 
-    from Gmail.list_labels().
-    
-    Args:
-        name: The name of the Label.
-        id: The ID of the label.
-
-    Attributes:
-        name (str): The name of the Label.
-        id (str): The ID of the label.
-
-    """
-
-    def __init__(self, name: str, id: str) -> None:
-        self.name = name
-        self.id = id
-
-    def __repr__(self) -> str:
-        return f'Label(name={self.name!r}, id={self.id!r})'
-
-    def __str__(self) -> str:
-        return self.name
-
-    def __hash__(self) -> int:
-        return hash(self.id)
-
-    def __eq__(self, other) -> bool:
-        if isinstance(other, str):
-            # Can be compared to a string of the label ID
-            return self.id == other
-        elif isinstance(other, Label):
-            return self.id == other.id
-        else:
-            return False
-
+    name: str
+    id: str
 
 INBOX      = Label('INBOX', 'INBOX')
 SPAM       = Label('SPAM', 'SPAM')
