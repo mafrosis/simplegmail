292ba3abd02e148780d2ba1395065d3ab2bc3643
Matt Black <github@mafro.net>
2024-07-06 10:27:50 +1000

    Port auth class from kuzmoyev/google-calendar-simple-api

--- gmsa/message.py
+++ gmsa/message.py
@@ -1,6 +1,7 @@
 from typing import List, Optional, Union
 
 from googleapiclient.errors import HttpError
+from google.oauth2.credentials import Credentials
 
 from gmsa import label
 from gmsa.attachment import Attachment
@@ -34,14 +35,14 @@ class Message:
     '''
     def __init__(
         self, service: 'googleapiclient.discovery.Resource',
-        creds: 'oauth2client.client.OAuth2Credentials', user_id: str, msg_id: str, thread_id: str,
+        credentials: Credentials, user_id: str, msg_id: str, thread_id: str,
         recipient: str, sender: str, subject: str, date: str, snippet: str, plain: Optional[str]=None,
         html: Optional[str]=None, label_ids: Optional[List[str]]=None,
         attachments: Optional[List[Attachment]]=None, headers: Optional[dict]=None,
         cc: Optional[List[str]]=None, bcc: Optional[List[str]]=None
     ):
         self.service = service
-        self.creds = creds
+        self.credentials = credentials
         self.user_id = user_id
         self.id = msg_id
         self.thread_id = thread_id
